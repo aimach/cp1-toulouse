c2dd9193b6e0cce2b34252c1390417424f869180
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2022-10-10 11:57:36 +0200

    Exercice JS terminé excepté les bonus

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,25 @@
+//CHANGEMENT DE L'AVATAR
 const avatar = document.querySelector('#avatar');
-
 avatar.addEventListener('click', () => {
     avatar.src = "image/avatar-bis.png";
 });
+
+//MODIFICATION DU NOM ET COULEUR
+const button = document.querySelector("button");
+const span = document.querySelector("#name");
+const backgroundPink = document.querySelectorAll(".pink-bg");
+const titlePink = document.querySelectorAll(".pink-text");
+const links = document.querySelectorAll("a");
+console.log(links)
+
+button.addEventListener('click', () => {
+    let colorUser = prompt('Enter a color')
+    let nameUser = prompt('Enter your name');
+    span.innerHTML = nameUser;
+    span.style.color = "white";
+    titlePink.forEach(element => element.style.color = colorUser)
+    backgroundPink.forEach(element => element.style.backgroundColor = colorUser)
+    links.forEach(element => element.style.textDecoration = none)
+    links.forEach(element => element.style.color = colorUser)
+
+})
